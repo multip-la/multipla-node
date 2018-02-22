e12f61e1d716e0d77179d58921690bf891a8835f
Anthony Chinwo <anthony.chinwo@gmail.com>
2018-02-22 22:53:41 +0000

    - switch to port 80 for heroku deploy

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ module.exports = app;
 const server = http.createServer(app)
 // Listen
 
-const port = 26116
+const port = 80
 console.log(`starting monitoring server on ${port}...`)
 server.listen(port)
 
